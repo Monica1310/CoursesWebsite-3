4a29f6a2bba868e7cae7d46c66108c713b5be6e3
Monica1310 <168436554+Monica1310@users.noreply.github.com>
2024-05-23 13:45:21 +0530

    Add files via upload

---
+++ CoursesWebsite-3/scripts/delete.js
@@ -0,0 +1,106 @@
+"use strict"
+window.onload=init;
+
+function init()
+{
+
+const urlParams = new URLSearchParams(location.search);
+
+
+
+	// location.search returns the query string part of the URL
+let cid = -1;
+if (urlParams.has("cid") === true)
+{
+   cid = urlParams.get("cid")
+   // call a method that fetches this course
+   getCourse(cid);
+   
+}
+
+
+}
+
+function getCourse(cid) {
+    fetch('http://localhost:8081/api/courses/' + cid)
+     .then(response => response.json())
+     .then(course => {
+        // this returns a single course!
+        const container = 
+           document.getElementById('courseContainerDiv');
+  
+        // display one course property in a <p>
+        const deptP = document.createElement('p');
+        deptP.textContent = `Dept: ${course.dept}`;
+        container.appendChild(deptP);
+    
+        // repeat for each field you want to display
+        const numberp = document.createElement('p');
+        numberp.textContent = `Course Number: ${course.courseNum}`;
+        container.appendChild(numberp);
+
+        const namep = document.createElement('p');
+        namep.textContent = `Course Name: ${course.courseName}`;
+        container.appendChild(namep);
+
+        const instructorp = document.createElement('p');
+        instructorp.textContent = `Instructor: ${course.instructor}`;
+        container.appendChild(instructorp);
+
+        const startDatep = document.createElement('p');
+        startDatep.textContent = `StartDate: ${course.startDate}`;
+        container.appendChild(startDatep);
+  
+        const numDaysp = document.createElement('p');
+        numDaysp.textContent = `numDays: ${course.numDays}`;
+        container.appendChild(numDaysp);
+  
+      })
+  
+     .catch(error => {
+        // handle errors that occurred during the fetch request
+      });
+   
+  }
+  
+
+function ondeletebtnclicked(e)
+  {
+    const urlParams = new URLSearchParams(location.search);
+    let cid = -1;
+    if (urlParams.has("cid") === true)
+    {
+       cid = urlParams.get("cid")
+       // call a method that fetches this course
+      
+       
+    }
+
+   fetch("http://localhost:8081/api/courses/" + cid, {
+      method: "DELETE",
+      headers: {
+         'Content-Type': 'application/json'
+       },
+       body: null
+   })
+   .then(response => {
+      alert("Course deleted");
+  }) 
+   .then(data => {
+       // If the DELETE is successful, display a message
+    //    let confirmationMessage = 
+    let confirmationMessage= document.getElementById("messageid");
+     confirmationMessage.innerHTML = "Course deleted";
+
+     console.log(data) 
+       //
+      // window.location.href="index.html";
+    })
+    .catch(err => {
+       // If the DELETE returns an error, display a message
+       let confirmationMessage = 
+          document.getElementById("messageid");
+       confirmationMessage.innerHTML = "Unexpected error.";
+  });
+
+  }
